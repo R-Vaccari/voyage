6c7f59cdd35523b8981486b605b50240aedf0f04
R-Vaccari <rodrigo.v.melo@hotmail.com>
2021-02-25 09:45:40 -0300

    Basic project

---
+++ app/src/main/java/com/rvapp/voyage/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.rvapp.voyage.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
