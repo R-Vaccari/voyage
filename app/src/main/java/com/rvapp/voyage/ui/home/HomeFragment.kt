add2c6859e83266b5e510067b5396956c5293364
R-Vaccari <rodrigo.v.melo@hotmail.com>
2021-02-26 08:49:56 -0300

    OkHttp impl, internet permission, successful request.

--- app/src/main/java/com/rvapp/voyage/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rvapp/voyage/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.rvapp.voyage.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.button.MaterialButton
 import com.rvapp.voyage.R
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +30,22 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        root.findViewById<MaterialButton>(R.id.import_bt).setOnClickListener {
+            val client = OkHttpClient()
+
+            val request = Request.Builder()
+                    .url("https://countries-cities.p.rapidapi.com/location/country/PT/city/list")
+                    .get()
+                    .addHeader("x-rapidapi-key", "73a22a72d3msh9a0f3e37accb5abp1b198ajsn7bdc106de086")
+                    .addHeader("x-rapidapi-host", "countries-cities.p.rapidapi.com")
+                    .build()
+
+            Thread {
+                val response = client.newCall(request).execute()
+                Log.d(null, response.body!!.string())
+            }.start()
+        }
+
         return root
     }
 }
