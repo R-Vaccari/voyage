2decf536b77420c377cf48ab617128d62f9abcb1
R-Vaccari <rodrigo.v.melo@hotmail.com>
2021-02-26 21:29:49 -0300

    API class. Working data observe from viewmodel with result from coroutine

--- app/src/main/java/com/rvapp/voyage/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rvapp/voyage/ui/home/HomeFragment.kt
@@ -1,24 +1,20 @@
 package com.rvapp.voyage.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 import com.rvapp.voyage.R
-import com.rvapp.voyage.model.entities.City
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.json.JSONArray
-import org.json.JSONObject
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
@@ -26,42 +22,16 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val editText: TextInputEditText = root.findViewById(R.id.text_home)
+        val tvCities = root.findViewById<MaterialTextView>(R.id.tv_cities_count)
+        homeViewModel.cities.observe(viewLifecycleOwner, Observer {
+            tvCities.text = it.toString()
         })
         root.findViewById<MaterialButton>(R.id.import_bt).setOnClickListener {
-            val client = OkHttpClient()
-
-            val request = Request.Builder()
-                    .url("https://countries-cities.p.rapidapi.com/location/country/PT/city/list")
-                    .get()
-                    .addHeader("x-rapidapi-key", "73a22a72d3msh9a0f3e37accb5abp1b198ajsn7bdc106de086")
-                    .addHeader("x-rapidapi-host", "countries-cities.p.rapidapi.com")
-                    .build()
-
-            Thread {
-                val response = client.newCall(request).execute()
-                val cities = parse(response.body!!.string())
-                Log.d(null, cities.toString())
-            }.start()
+            GlobalScope.launch { homeViewModel.getCitiesByCountryCode(editText.text.toString()) }
         }
-
         return root
-    }
-
-    fun parse(body: String) : MutableList<City> {
-        val cities = mutableListOf<City>()
-        val array = JSONObject(body).getJSONArray("cities")
-        for (i in 0 until array.length()) {
-            val jsonObject = array.getJSONObject(i)
-            val city = City(jsonObject.optInt("geonameid"), jsonObject.optString("name"),
-                jsonObject.optString("latitude"), jsonObject.optString("longitude"), jsonObject.optInt("population"), jsonObject.optString("timezone"))
-            cities.add(city)
-        }
-        return cities
     }
 }
--- app/src/main/java/com/rvapp/voyage/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/rvapp/voyage/ui/home/HomeViewModel.kt
@@ -3,6 +3,10 @@ package com.rvapp.voyage.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.rvapp.voyage.model.api.CountriesCitiesAPI
+import com.rvapp.voyage.model.entities.City
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +14,13 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _cities = MutableLiveData<Int>()
+    val cities: LiveData<Int> = _cities
+
+    fun getCitiesByCountryCode(countryCode: String): MutableList<City> {
+        val cities = CountriesCitiesAPI.getCitiesFromCountryCode(countryCode)
+        _cities.postValue(cities.size)
+        return cities
+    }
 }
